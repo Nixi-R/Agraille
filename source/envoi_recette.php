aa15e2d61243cb37325048db18dbd77552756a6a
nixovii <vsrnixo@gmail.com>
2022-06-08 10:54:53 +0200

    !

--- source/envoi_recette.php
+++ source/envoi_recette.php
@@ -1,40 +1,31 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="UTF-8">
-    <link rel="stylesheet" href="../css/normalize.css">
-    <link rel="stylesheet" href="../css/envoi_recette.css">
-    <title>Agraille|Validation de votre recette</title>
-</head>
-    <body>
 <?php
     session_start();
     $bdd = new PDO ('mysql:host=localhost;dbname=agrailledb;charset=utf8','root','', [PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES latin1 COLLATE latin1_general_ci",PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
 
     switch (null) {
         case $_POST["title"]:
-            print("<h5>Erreur: Vous n'avez pas entré de titre pour votre recette.</h5><a href = http://localhost/Agraille/source/redaction_recette.php>Retourner à la page de création</a>");
+            echo "<h5>Erreur: Vous n'avez pas entré de titre pour votre recette.</h5><a href = http://localhost/Agraille/source/redaction_recette.php>Retourner à la page de création</a>";
             break;
         case $_POST["description"]:
-            print("<h5>Erreur: Vous n'avez pas entré de description pour votre recette.</h5><a href = http://localhost/Agraille/source/redaction_recette.php>Retourner à la page de création</a>");
+            echo "<h5>Erreur: Vous n'avez pas entré de description pour votre recette.</h5><a href = http://localhost/Agraille/source/redaction_recette.php>Retourner à la page de création</a>";
             break;
         case $_POST["methode_cuisson"]:
-            print("<h5>Erreur: Vous n'avez pas entré de méthode de cuisson pour votre recette.</h5><a href = http://localhost/Agraille/source/redaction_recette.php>Retourner à la page de création</a>");
+            echo "<h5>Erreur: Vous n'avez pas entré de méthode de cuisson pour votre recette.</h5><a href = http://localhost/Agraille/source/redaction_recette.php>Retourner à la page de création</a>";
             break;
         case $_POST["step_1"]:
-            print("<h5>Erreur: Vous n'avez pas entré d'étapes pour votre recette.</h5><a href = http://localhost/Agraille/source/redaction_recette.php>Retourner à la page de création</a>");
+            echo "<h5>Erreur: Vous n'avez pas entré d'étapes pour votre recette.</h5><a href = http://localhost/Agraille/source/redaction_recette.php>Retourner à la page de création</a>";
             break;
         case $_POST["temps_realisation"]:
-            print("<h5>Erreur: Vous n'avez pas entré de temps de réalisation pour votre recette.</h5><a href = http://localhost/Agraille/source/redaction_recette.php>Retourner à la page de création</a>");
+            echo "<h5>Erreur: Vous n'avez pas entré de temps de réalisation pour votre recette.</h5><a href = http://localhost/Agraille/source/redaction_recette.php>Retourner à la page de création</a>";
             break;
         case $_POST["difficulte"]:
-            print("<h5>Erreur: Vous n'avez pas entré de difficulté pour votre recette.</h5><a href = http://localhost/Agraille/source/redaction_recette.php>Retourner à la page de création</a>");
+            echo "<h5>Erreur: Vous n'avez pas entré de difficulté pour votre recette.</h5><a href = http://localhost/Agraille/source/redaction_recette.php>Retourner à la page de création</a>";
             break;
         case $_POST["type_recette"]:
-            print("<h5>Erreur: Vous n'avez pas entré de type de recette pour votre recette.</h5><a href = http://localhost/Agraille/source/redaction_recette.php>Retourner à la page de création</a>");
+            echo "<h5>Erreur: Vous n'avez pas entré de type de recette pour votre recette.</h5><a href = http://localhost/Agraille/source/redaction_recette.php>Retourner à la page de création</a>";
             break;
         case $_POST["recette_image"]:
-            print("<h5>Erreur: Vous n'avez pas entré d'illustration pour votre recette.</h5><a href = http://localhost/Agraille/source/redaction_recette.php>Retourner à la page de création</a>");
+            echo "<h5>Erreur: Vous n'avez pas entré d'illustration pour votre recette.</h5><a href = http://localhost/Agraille/source/redaction_recette.php>Retourner à la page de création</a>";
             break;
     }
 
@@ -57,7 +48,8 @@
 
     $sql = "INSERT INTO recette (id, nom, description, date_publication, etape, temps_realisation, ingredients, illustration, methode_cuisson, auteur, type, difficulte) VALUES (30,'$title','$description','$date','$str_etape','$temps_realisation','ouais','$recette_image','$methode_cuisson','$auteur','$type_recette','$difficulte');";
     $insert_sql = $bdd->prepare($sql);
-    //$insert_sql->execute();
+    $insert_sql->execute();
+
+    print("Nous avons reçu votre recette !");
+
 ?>
-    </body>
-</html>
