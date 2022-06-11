a8a7e096c730938bb2465af716f95d36d8590e46
Lucien-02 <81372035+Lucien-02@users.noreply.github.com>
2022-06-11 15:27:01 +0200

    filtrage interface

--- source/page_filtrage.php
+++ source/page_filtrage.php
@@ -35,6 +35,7 @@ catch (Exception $e)
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="../css/normalize.css">
+    <link rel="stylesheet" href="../css/filtrage.css">
     <link rel="stylesheet" href="../css/index.css">
     <link rel="stylesheet" href="../css/navbar.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
@@ -134,7 +135,7 @@ catch (Exception $e)
                     <p class="filtreIcons flexCentre">🍒</p>
                     <div class="filtreTexte flexCentre">Par ingrédient<input type="text" name="ingredients"></div>
                 </div>
-                <input type="submit" value="Chercher">
+                <div class="bouton flexCentre"><input type="submit" value="Chercher"></div>
             </form>
             </div>
             <?php
